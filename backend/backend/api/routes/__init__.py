8330b95c91fa97bd7b2f2089322d405000b87f75
roeefeingold <roee2912@gmail.com>
2025-10-02 19:55:39 +0300

    CRITICAL: Remove duplicate backend/backend/ directory causing deployment issues

--- backend/backend/api/routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .auth import auth_bp
-from .people import people_bp
-from .tasks import tasks_bp
-from .csv import csv_bp
-from .telegram import telegram_bp
-
-__all__ = ['auth_bp', 'people_bp', 'tasks_bp', 'csv_bp', 'telegram_bp']
--- backend/backend/dal/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .person import Person
-from .task import Task
-from .event import Event
-
-__all__ = ['User', 'Person', 'Task', 'Event']
